e868951ccc181bbe9b59ce1e523f3cf652fb0255
NekoMaya <82465137+CodelessHeart@users.noreply.github.com>
2021-04-26 21:15:02 +0300

    Update ASCII-Crafter.py

--- ASCII-Crafter.py
+++ ASCII-Crafter.py
@@ -36,25 +36,30 @@ def figfont(text , font2):
 	result = pyfiglet.figlet_format(text, font = font2 )
 	print(result)
 def craft():
+	ch1 = int(input("Время до закрытия : "))
 	text2 = input("Текст для вывода: ")
 	fig(text2)
-	time.sleep(5)
+	time.sleep(ch1)
+	raise SystemExit
 def craftfont():
+	ch1 = int(input("Время до закрытия : "))
 	text2 = input("Текст для вывода: ")
 	fig(text2)
 	time.sleep(1)
 	fonter = input("Фонт какой? : ")
 	if fonter == "Никакой":
 		print("Слыш б**, я ща тебя на вертель накручу, и в шаурму закатаю, шрифт ты никакой бл*.")
-		time.sleep(5)
+		time.sleep(ch1)
 		raise SystemExit
 	if fonter == "Украина":
 		print("СЛАВА УКРАИНЕ! ГЕРОЯМ СЛАВА!")
-		time.sleep(5)
+		time.sleep(ch1)
 		raise SystemExit
-	if fonter == "Зайцев Кирилл":
+	if fonter == "vk.com/hate3 Zaytsev Kirill":
 		print("ДА ПРИКИНЬ ЭТУ ПРОГУ НАПИСАЛ Я!")
-		time.sleep(5)
+		time.sleep(ch1)
 		raise SystemExit
 	figfont(text2 , fonter)
+	time.sleep(ch1)
+	raise SystemExit
 menu()
